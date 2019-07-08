cdd61b0978644225fc5064cfb7ca7e1ede4e6394
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-08 16:34:48 -0700

    :construction: WIP

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+// models
+// const urlModel = require('./models/mongooseUrl')
+
 // Routers
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/api/shorturl/new', urlShortRouter)
+app.use('/api/shorturl', urlShortRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const router = express.Router()
-// example: POST [project_url]/api/shorturl/new - https://www.google.com
+const mongoose = require('mongoose')
+const { connection, Schema } = mongoose
+const uri =
+  'mongodb+srv://sandrohp88:wy4Pf2fDECCC7znj@freecodecampapimicroservices-qsq8r.mongodb.net/freecodecamp?retryWrites=true&w=majority'
+mongoose.connect(uri, { useNewUrlParser: true }).catch(console.error)
+// create the Schema
+const UrlSchema = new Schema(
+  { original: String},
+  { collection: 'urlshortener' }
+)
 
-router.post('/', function(req, res, next) {
-  const { url } = req.body
-  res.send({ original: url })
-})
+const Url = mongoose.model('Url', UrlSchema)
+
+router
+  .post('/new', function(req, res, next) {
+    const { url } = req.body
+    // TODO verify url
+    const newUrl = new Url({ original: url })
+    newUrl.save(err => {
+      if (err) return console.log(err)
+      res.send({ original: url })
+    })
+  })
+  .route('/:id')
+  .get((req, res, next) => {
+    const id = req.params.id
+    Url.findById(id, (err, url) => {
+      if (err) return console.log(err)
+      res.send(url)
+      console.log(url.original)
+    })
+  })
 module.exports = router
