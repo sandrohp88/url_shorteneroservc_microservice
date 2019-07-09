47e8500cacd5758f6d82434cf58927523e47fb36
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-09 15:31:06 -0700

    Add url check

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -2,26 +2,29 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const { connection, Schema } = mongoose
+const dns = require('dns')
 const uri =
   'mongodb+srv://sandrohp88:wy4Pf2fDECCC7znj@freecodecampapimicroservices-qsq8r.mongodb.net/freecodecamp?retryWrites=true&w=majority'
 mongoose.connect(uri, { useNewUrlParser: true }).catch(console.error)
 // create the Schema
 const UrlSchema = new Schema(
-  { original: String},
+  { original: String },
   { collection: 'urlshortener' }
 )
 
 const Url = mongoose.model('Url', UrlSchema)
 
 router
-  .post('/new', function(req, res, next) {
+  .post('/new', async function(req, res, next) {
     const { url } = req.body
     // TODO verify url
-    const newUrl = new Url({ original: url })
-    newUrl.save(err => {
-      if (err) return console.log(err)
-      res.send({ original: url })
+    dns.lookup(url.split('//')[1], (error, addresses, family) => {
+      console.log(error)
+      if (error) return res.send({ error: 'invalid URL' })
     })
+    const newUrl = new Url({ original: url })
+    const response = await newUrl.save()
+    res.send({ original_url: response.original, short_url: response._id })
   })
   .route('/:id')
   .get((req, res, next) => {
